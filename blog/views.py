2845ae755a84722ec100bba57e82ee7ed2d4c3cb
Loannes <loannes.dev@gmail.com>
2022-09-16 16:25:00 +0900

    오류 수정

--- blog/views.py
+++ blog/views.py
@@ -5,17 +5,22 @@ from rest_framework import viewsets
 from rest_framework import permissions
 
 from .permissions import OnlyOwner
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 import pdb
 		
 # Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
-    permission_classes = [OnlyOwner]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, OnlyOwner]
 
     queryset = Post.objects.all()
     serializer_class = BlogSerializer
    
-    def get_queryset(self):                                          
-        return super().get_queryset().filter(id=self.request.user.id)
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
