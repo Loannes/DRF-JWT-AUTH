070436f76fbedb81acf572f141e47b098661dcd0
Loannes <loannes.dev@gmail.com>
2022-09-19 15:45:06 +0900

    blog에 검색 조건 추가

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,6 @@ from .permissions import OnlyOwner
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-
-import pdb
 		
 # Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
@@ -20,7 +18,19 @@ class BlogViewSet(viewsets.ModelViewSet):
     serializer_class = BlogSerializer
    
     def get_queryset(self):
-        return super().get_queryset().filter(user=self.request.user)
+        search_text = self.request.query_params.get('search_text', '')
+        start_date = self.request.query_params.get('start_date', '')
+        end_date = self.request.query_params.get('end_date', '')
+
+        qs = super().get_queryset()
+
+        if search_text:
+            qs = qs.filter(title__icontains=search_text)
+
+        if start_date and end_date:
+            qs = qs.filter(created_at__range=[start_date, end_date])
+
+        return qs.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
